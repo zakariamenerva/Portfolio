f17e3411ae4024573c873491470f62db35077920
zakaria menerva <zakariamenerva@gmail.com>
2024-11-07 20:00:42 +0100

    add component navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,10 @@ import zakaria from "../asset/zakaria.jpg";
 
 const Home = () => {
   return (
-    <div name="home" className="w-full px-10 flex justify-between h-screen bg-[#0a192f]">
+    <div
+      name="home"
+      className="w-full px-10 flex justify-between h-screen bg-[#0a192f]"
+    >
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600">Hi, my name is</p>
@@ -15,15 +18,22 @@ const Home = () => {
           I'm a Full Stack Developer,
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I'm a full-stack web developer, dedicated to creating and sometimes designing high-quality digital experiences. My current focus is on building responsive web applications that provide engaging user interactions.
+          I'm a full-stack web developer, dedicated to creating and sometimes
+          designing high-quality digital experiences. My current focus is on
+          building responsive web applications that provide engaging user
+          interactions.
         </p>
         <div>
           <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            <a target={"_blank"} href="https://drive.google.com/file/d/1iWn-MsmPaHrQXU8Z3mmyAYJLaeipjAPU/view?usp=sharing">Get Resume</a>
+            <a
+              target={"_blank"}
+              href="https://drive.google.com/file/d/1iWn-MsmPaHrQXU8Z3mmyAYJLaeipjAPU/view?usp=sharing"
+            >
+              Get Resume
+            </a>
             <span className="group-hover:rotate-90 duration-300 ">
               <HiArrowNarrowRight className="ml-3" />
             </span>
-
           </button>
         </div>
       </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaFacebook
+} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+       
+      </div>
+      {/* Menu */}
+      <ul className="hidden md:flex">
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/* Social icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              href="https://www.linkedin.com/in/zakaria-menerva-9b2961290/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              href="https://github.com/zakariamenerva"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+            <a
+              href="mailto:zakariamenerva@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              href="https://drive.google.com/file/d/1iWn-MsmPaHrQXU8Z3mmyAYJLaeipjAPU/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
