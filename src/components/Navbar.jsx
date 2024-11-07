3c48eaf63b17d021bf727fce47e7a4c8e89a079b
zakaria menerva <zakariamenerva@gmail.com>
2024-11-07 20:18:52 +0100

    add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import {
 } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-
+import logo from "../asset/logo.png";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
@@ -17,7 +17,9 @@ const Navbar = () => {
 
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-    
+     <div>
+        <img src={logo} alt="Logo Image" style={{ width: "160px" }} />
+      </div>
       {/* Menu */}
       <ul className="hidden md:flex">
         <li>
