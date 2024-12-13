a37ed3dfd82eaef8a8f1f19666787a8a4b251766
zakaria menerva <zakariamenerva@gmail.com>
2024-12-13 16:29:55 +0100

    add Work1

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import WorkImg from "../asset/work1.png";
+import WorkImg from "../asset/Work1.png";
 
 const Work = () => {
   return (
@@ -11,80 +11,39 @@ const Work = () => {
           </p>
           <p className="py-6">Check out some of my recent work</p>
         </div>
-        {/* container */}
+        {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-1xl font-bold text-white tracking-wider">
-                Product management system
-              </span>
-              <div className="pt-8 text-center">
-                <a href="#" target={"_blank"}>
-                  <button className="text-center rounded-olg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="#" target={"_blank"}>
-                  <button className="text-center rounded-olg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+          {[...Array(3)].map((_, index) => (
+            <div
+              key={index}
+              style={{
+                backgroundImage: `url(${WorkImg})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* Hover Effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-1xl font-bold text-white tracking-wider">
+                  Product management system
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://project-1303-sage.vercel.app/" target={"_blank"} rel="noopener noreferrer">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="#" target={"_blank"} rel="noopener noreferrer">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-1xl font-bold text-white tracking-wider">
-                Product management system
-              </span>
-              <div className="pt-8 text-center">
-                <a href="#" target={"_blank"}>
-                  <button className="text-center rounded-olg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="#" target={"_blank"}>
-                  <button className="text-center rounded-olg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-1xl font-bold text-white tracking-wider">
-                Product management system
-              </span>
-              <div className="pt-8 text-center">
-                <a href="#" target={"_blank"}>
-                  <button className="text-center rounded-olg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="#" target={"_blank"}>
-                  <button className="text-center rounded-olg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
+          ))}
         </div>
       </div>
     </div>
